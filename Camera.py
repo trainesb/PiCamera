7d4224aa014e0cc2b9ac7cfb077992ecae1d9826
trainesb <37374492+trainesb@users.noreply.github.com>
2019-11-26 15:16:15 -0500

    Add files via upload

---
+++ Camera.py
@@ -0,0 +1,90 @@
+########################################################################
+#
+# Author: Ben Traines
+# Date: 11/22/2019
+# Description: 
+#
+########################################################################
+
+from io import BytesIO
+from time import sleep
+
+import picamera
+from picamera import PiCamera
+from PIL import Image
+
+class Camera():
+	
+	self.CAM_WIDTH = 1024
+	self.CAM_HEIGHT = 768
+	self.ZOOM = 50
+	self.LED = True
+	
+	def __init__(slef):
+		self.camera = PiCamera()
+		self.camera.resolution = (self.CAM_WIDTH, self.CAM_HEIGHT)
+		self.camera.start_preview()
+		# Camera warm-up time
+		sleep(2)
+		
+		
+		self.camera.framerate = 10
+		self.rawCapture = PiRGBArray(self.camera, size=(self.CAM_WIDTH, self.CAM_HEIGHT))
+		self.rawCapture.truncate(0)
+		
+	def screen_shot(self, fileName):
+		try:
+			self.camera.capture(fileName + '.jpg')
+			return True
+		except:
+			return False
+			
+	def capture_stream(self):
+		try:
+			# Create in-memory stream
+			stream = BytesIO()
+			self.camera.capture(stream, 'jpeg')
+			return stream
+		except:
+			return False
+			
+	def capture_to_PIL(self):
+		stream = self.capture_stream()
+		# "Rewind" the stream to the beginning
+		stream.seek(0)
+		return Image.open(stream)
+		
+	def capture_size(self, file, width, height):
+		self.camera.capture(file+'.jpg', resize=(width, height))
+		
+	def capture_timelapse(self, time):
+		for filename in self.camera.capture_contnuous('img{counter:03d}.jpg'):
+			print('Captured %s' % filename)
+			sleep(time)
+			
+	def low_light_mode(self, filename):
+		self.camera.framerate = Fraction(1, 6)
+		self.camera.sensor_mode = 3
+		self.camera.shutter_speed = 6000000
+		self.camera.iso = 800
+		sleep(30)
+		self.camera.exposure_mode = 'off'
+		self.camera.capture(filename + '.jpg')
+		
+	def record_video(self, fileName, time):
+		self.camera.start_recording(filename+'.h264')
+		self.camera.wait_recording(time)
+		self.camera.stop_recording()
+		
+	def LED_switch(self):
+		if self.LED:
+			self.LED = False
+		else:
+			self.LED = True
+		self.camera.led = self.LED
+		
+	def zoom(self, n):
+		camera.zoom((n/100., n/100., 0.5, 0.5)
+		self.zoom = n
+		return self.zoom
+	
